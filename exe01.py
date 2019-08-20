a9c001ca57528980fa8205c6319da16db0b8d0fa
khonnsu <51890424+khonnsu@users.noreply.github.com>
2019-08-20 12:04:11 -0300

    Add files via upload

--- exe01.py
+++ exe01.py
@@ -1,6 +1,9 @@
 #calcula e imprime ano em que usuário terá 100 anos de idade
-import datetime 					#usa módulo para conseguir ano atual
-name = input("escreva seu nome: ")			#lê string após imprimir a frase descrita
-age = int(input("escreva sua idade: "))			#faz o mesmo da anterior porém transforma em int
-year = datetime.datetime.now().year + (100 - age)	#calcula ano atual + anos que faltam para completar 100
-print(name+" tera 100 anos em "+str(year))		#imprime concatenando strings, para o ano é feito cast
+import datetime 					# usa módulo para conseguir ano atual
+name = input("escreva seu nome: ")			# lê string após imprimir a frase descrita
+age = int(input("escreva sua idade: "))			# faz o mesmo da anterior porém transforma em int
+loops = int(input("numero de copias da resposta: "))	# recebe quantas vezes repetir mensagem final
+year = datetime.datetime.now().year + (100 - age)	# calcula ano atual + anos que faltam para completar 100
+for x in range (0,loops):				# laco usando o valor pedido
+	print(name+" tera 100 anos em "+str(year))	# imprime concatenando strings, para o ano é feito cast
+
